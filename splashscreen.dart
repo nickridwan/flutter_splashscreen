620d66150fba1c1d0b493b051a917e59da4b1df4
Ridwan Hanif <99933836+nickridwan@users.noreply.github.com>
2023-01-19 14:40:25 +0700

    Update splashscreen.dart

--- splashscreen.dart
+++ splashscreen.dart
@@ -1,13 +1,51 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import '../../theme.dart';
+import '../screens/home_page.dart';
+import 'animation.dart';
 
 class Splash extends StatefulWidget {
   @override
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> {
+class _SplashState extends State<Splash> with TickerProviderStateMixin {
+  AnimationController? scaleController;
+  Animation<double>? scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scaleController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500))
+      ..addStatusListener(
+        (status) {
+          if (status == AnimationStatus.completed) {
+            Navigator.push(
+              context,
+              AnimatingRoute(
+                route: HomePage(),
+                page: HomePage(),
+              ),
+            );
+            Timer(
+              const Duration(milliseconds: 300),
+              () {
+                scaleController!.reset();
+              },
+            );
+          }
+        },
+      );
+
+    scaleAnimation =
+        Tween<double>(begin: 0.0, end: 10.0).animate(scaleController!);
+  }
+
   @override
   void dispose() {
+    scaleController!.dispose();
     super.dispose();
   }
 
@@ -16,48 +54,85 @@ class _SplashState extends State<Splash> {
     return Builder(builder: (context) {
       Future loading() async {
         await Future.delayed(const Duration(seconds: 2), () {
-          Navigator.pushReplacementNamed(context, '/login');
+          setState(() {
+            scaleController!.forward();
+          });
         });
       }
 
       return Scaffold(
-        body: Container(
-          padding: const EdgeInsets.all(10.0),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FutureBuilder(
-                future: loading(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.data == null) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          child: Image.asset(
-                            "assets/verify.png",
-                            scale: 2,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        Text(
-                          "INSTA APPLICATION",
-                          style: Style.whiteTextStyle.copyWith(fontSize: 20),
-                        ),
-                      ],
-                    );
-                  }
-                  return Container();
-                },
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FutureBuilder(
+                    future: loading(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.data == null) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              child: Image.asset(
+                                "assets/verify.png",
+                                scale: 2,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            Text(
+                              "INSTA APPLICATION",
+                              style:
+                                  Style.whiteTextStyle.copyWith(fontSize: 20),
+                            ),
+                          ],
+                        );
+                      }
+                      return Container();
+                    },
+                  ),
+                ],
               ),
-            ],
+            ),
+            Center(
+              child: Center(
+      child: InkWell(
+        onTap: () {
+          scaleController!.forward();
+        },
+        child: Container(
+          width: 100,
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Colors.transparent,
           ),
+          child: AnimatedBuilder(
+            animation: scaleAnimation!,
+            builder: (c, child) => Transform.scale(
+              scale: scaleAnimation!.value,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColor.kDarkBackgroundPrimaryColor,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+            )
+          ],
         ),
       );
     });
   }
+
+ 
 }
